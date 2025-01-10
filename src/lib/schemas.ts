ba91cc357dd64eefa5d3693360a6cd6ff5017792
Ryu Rabino <ryebytescode@proton.me>
2025-01-10 21:44:12 +0800

    feat: add regex validation for sign up schema

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,4 +1,5 @@
 import { type ZodType, z } from 'zod'
+import patterns from './patterns'
 
 export const SignUpSchema: ZodType<SignUpFields> = z
   .object({
@@ -6,32 +7,48 @@ export const SignUpSchema: ZodType<SignUpFields> = z
       .string({
         required_error: 'Required',
       })
-      .min(1, 'Required'),
+      .min(1, 'Required')
+      .regex(patterns.name, 'Invalid name'),
     lastName: z
       .string({
         required_error: 'Required',
       })
-      .min(1, 'Required'),
+      .min(1, 'Required')
+      .regex(patterns.name, 'Invalid name'),
     email: z
       .string({
         required_error: 'Required',
       })
       .min(1, 'Required')
+      .regex(patterns.email, 'Invalid email format')
       .optional(),
     phone: z
       .string({
         required_error: 'Required',
       })
       .regex(
-        /((\+[0-9]{2})|0)[.\- ]?9[0-9]{2}[.\- ]?[0-9]{3}[.\- ]?[0-9]{4}/,
+        patterns.phone,
         'Invalid format. Should be in the form 0912 345 6789'
       )
       .optional(),
     password: z
       .string({
-        required_error: 'Required',
+        required_error: 'Requirsed',
       })
-      .min(8)
+      .min(8, 'Must be 8 characters long')
+      .regex(
+        patterns.password.oneUpperCase,
+        'Must contain at least one uppercase letter'
+      )
+      .regex(
+        patterns.password.oneLowerCase,
+        'Must contain at least one lowercase letter'
+      )
+      .regex(patterns.password.oneDigit, 'Must contain at least one number')
+      .regex(
+        patterns.password.oneSpecialChar,
+        'Must contain at least one special character (.#?!@$%^&*-)'
+      )
       .optional(),
     mode: z.enum(['email', 'phone']),
   })
