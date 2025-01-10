cae6cf4e4ade712a2a3a7c34bee3cafbbdedac41
Ryu Rabino <ryebytescode@proton.me>
2025-01-10 10:37:03 +0800

    feat(routes): add the main layout

---
+++ src/app/(main)/_layout.tsx
@@ -0,0 +1,13 @@
+import { ClStack } from '@/components/navigation/ClStack'
+import { useRenderCount } from '@/hooks/useRenderCount'
+import { Stack } from 'expo-router'
+
+export default function MainLayout() {
+  useRenderCount('MainLayout')
+
+  return (
+    <ClStack id="main">
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </ClStack>
+  )
+}
