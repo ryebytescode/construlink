1061c3bb9d04b78df8f15bcfd1fc11e40eff80a9
Ryu Rabino <ryebytescode@proton.me>
2025-01-10 18:51:28 +0800

    fix: make email, phone and password optional and add mode field to the SignUpFields interface

--- src/types/fields.d.ts
+++ src/types/fields.d.ts
@@ -1,9 +1,10 @@
 interface SignUpFields {
   firstName: string
   lastName: string
-  email: string
-  phone: string
-  password: string
+  email?: string
+  phone?: string
+  password?: string
+  mode: 'email' | 'phone'
 }
 
 interface SignInFields {
