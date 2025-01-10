37710d77d5e1c7e829a1884b65a27679889d6d12
Ryu Rabino <ryebytescode@proton.me>
2025-01-10 13:48:37 +0800

    chore: add 'mode' state into the 'auth' store

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -3,13 +3,17 @@ import { create } from 'zustand'
 
 type State = {
   role: Role | null
+  mode: 'signin' | 'signup' | null
 }
 
 type Action = {
   setRole: (role: Role | null) => void
+  setMode: (mode: State['mode']) => void
 }
 
 export const useAuthStore = create<State & Action>()((set) => ({
   role: null,
+  mode: null,
   setRole: (role) => set({ role }),
+  setMode: (mode) => set({ mode }),
 }))
