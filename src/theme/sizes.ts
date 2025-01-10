f831742fb6ae61c1835ad5199d51777c967a53b3
Ryu Rabino <ryebytescode@proton.me>
2025-01-10 09:03:50 +0800

    feat: add constants for common elements and styling properties like avatars, icons, border radius, border width

---
+++ src/theme/sizes.ts
@@ -0,0 +1,45 @@
+const avatar = {
+  xs: 24,
+  sm: 32,
+  md: 48,
+  lg: 64,
+  xl: 96,
+  "2xl": 128,
+};
+
+const icon = {
+  xs: 12,
+  sm: 16,
+  md: 24,
+  lg: 32,
+  xl: 48,
+  "2xl": 64,
+  "3xl": 96,
+  "4xl": 128,
+};
+
+const borderWidth = {
+  none: 0,
+  thin: 1,
+  medium: 2,
+  thick: 4,
+  extraThick: 8,
+};
+
+const radius = {
+  none: 0,
+  xs: 2,
+  sm: 4,
+  md: 6,
+  lg: 8,
+  xl: 12,
+  "2xl": 16,
+  full: 9999,
+};
+
+export default {
+  avatar,
+  borderWidth,
+  icon,
+  radius,
+};
