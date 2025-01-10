982d9cd5e37e5e637f096360a9842bd966d219f9
Ryu Rabino <ryebytescode@proton.me>
2025-01-10 16:49:12 +0800

    feat(components): add text input controlled by react-hook-form

---
+++ src/components/controlled/ControlledTextInput.tsx
@@ -0,0 +1,59 @@
+import { resolveColor } from '@/helpers/resolveColor'
+import { useAppStore } from '@/stores/app'
+import { Sizes, Spacing } from '@/theme'
+import { MaterialIcons as Icon } from '@expo/vector-icons'
+import type { ComponentProps } from 'react'
+import {
+  type FieldValues,
+  type UseControllerProps,
+  useController,
+} from 'react-hook-form'
+import { View } from 'react-native'
+import { ClErrorMessage } from '../ClErrorMessage'
+import { ClTextInput } from '../ClTextInput'
+
+type TextInputProps = ComponentProps<typeof ClTextInput>
+
+interface ControlledTextInputProps {
+  textInputOptions?: Partial<
+    Omit<TextInputProps, 'onBlur' | 'onChange' | 'value' | 'valid' | 'disabled'>
+  >
+}
+
+export const ControlledTextInput = <TFields extends FieldValues>({
+  textInputOptions,
+  ...controllerProps
+}: ControlledTextInputProps & UseControllerProps<TFields>) => {
+  const { field, fieldState } = useController(controllerProps)
+  const colors = useAppStore((state) => state.colors)
+
+  return (
+    <View style={{ gap: Spacing[1] }}>
+      <ClTextInput
+        onChangeText={field.onChange}
+        {...field}
+        valid={!fieldState.invalid}
+        {...textInputOptions}
+      />
+      {fieldState.invalid && fieldState.error?.message && (
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            gap: Spacing[1],
+          }}
+        >
+          <Icon
+            name="error"
+            size={Sizes.icon.sm}
+            color={resolveColor(
+              colors.states.danger[300],
+              colors.states.danger[400]
+            )}
+          />
+          <ClErrorMessage message={fieldState.error?.message} />
+        </View>
+      )}
+    </View>
+  )
+}
