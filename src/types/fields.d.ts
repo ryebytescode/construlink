dec487f8fdd702e3d976963997a028af903d4b3f
Ryu Rabino <ryebytescode@proton.me>
2025-01-10 11:55:59 +0800

    chore: add field types

---
+++ src/types/fields.d.ts
@@ -0,0 +1,13 @@
+interface SignUpFields {
+  firstName: string
+  lastName: string
+  email: string
+  phone: string
+  password: string
+}
+
+interface SignInFields {
+  email: string
+  phone: string
+  password: string
+}
