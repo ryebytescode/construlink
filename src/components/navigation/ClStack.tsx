747a6c865f65074601f968e086693c3fa9381dad
Ryu Rabino <ryebytescode@proton.me>
2025-01-10 10:15:11 +0800

    feat(components): add custom stack navigator

---
+++ src/components/navigation/ClStack.tsx
@@ -0,0 +1,45 @@
+import { useRenderCount } from '@/hooks/useRenderCount'
+import { useAppStore } from '@/stores/app'
+import { Typo } from '@/theme'
+import { Stack } from 'expo-router'
+import type { ComponentProps } from 'react'
+import { ClBackArrow } from '../ClBackArrow'
+
+type ClStackProps = Omit<ComponentProps<typeof Stack>, 'id'> & { id: string }
+
+export function ClStack({
+  id,
+  children,
+  screenOptions,
+  ...rest
+}: ClStackProps) {
+  useRenderCount(`ClStack: ${id}`)
+
+  const colors = useAppStore((state) => state.colors)
+
+  return (
+    <Stack
+      screenOptions={{
+        animation: 'slide_from_right',
+        contentStyle: {
+          backgroundColor: colors.background,
+        },
+        headerStyle: {
+          backgroundColor: colors.background,
+        },
+        headerTintColor: colors.primaryText,
+        headerShadowVisible: false,
+        headerLeft: (props) => <ClBackArrow {...props} />,
+        headerBackVisible: false,
+        headerTitleStyle: {
+          ...Typo.fontMap.bold,
+          color: colors.primaryText,
+        },
+        ...screenOptions,
+      }}
+      {...rest}
+    >
+      {children}
+    </Stack>
+  )
+}
