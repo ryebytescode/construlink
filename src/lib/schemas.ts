3e1adad974420ce3e17f2050bf27e6bed86eafc6
Ryu Rabino <ryebytescode@proton.me>
2025-01-11 23:09:12 +0800

    fix: use discriminated union instead of refine

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,96 +1,78 @@
 import { type ZodType, z } from 'zod'
 import patterns from './patterns'
 
-export const SignUpSchema: ZodType<SignUpFields> = z
-  .object({
-    firstName: z
-      .string({
-        required_error: 'Required',
-      })
-      .min(1, 'Required')
-      .regex(patterns.name, 'Invalid name'),
-    lastName: z
-      .string({
-        required_error: 'Required',
-      })
-      .min(1, 'Required')
-      .regex(patterns.name, 'Invalid name'),
-    email: z
-      .string({
-        required_error: 'Required',
-      })
-      .min(1, 'Required')
-      .regex(patterns.email, 'Invalid email format')
-      .optional(),
-    phone: z
-      .string({
-        required_error: 'Required',
-      })
-      .regex(
-        patterns.phone,
-        'Invalid format. Should be in the form 0912 345 6789'
-      )
-      .optional(),
-    password: z
-      .string({
-        required_error: 'Required',
-      })
-      .min(8, 'Must be 8 characters long')
-      .regex(
-        patterns.password.oneUpperCase,
-        'Must contain at least one uppercase letter'
-      )
-      .regex(
-        patterns.password.oneLowerCase,
-        'Must contain at least one lowercase letter'
-      )
-      .regex(patterns.password.oneDigit, 'Must contain at least one number')
-      .regex(
-        patterns.password.oneSpecialChar,
-        'Must contain at least one special character (.#?!@$%^&*-)'
-      )
-      .optional(),
-    mode: z.enum(['email', 'phone']),
-  })
-  .refine((data) => (data.mode === 'email' ? !!data.email : !!data.phone), {
-    message: 'Required',
-    path: ['email', 'phone'],
-  })
-  .refine((data) => (data.mode === 'email' ? !!data.password : true), {
-    message: 'Required',
-    path: ['password'],
-  })
+const staticFields = {
+  firstName: z.string().min(1, 'Required').regex(patterns.name, 'Invalid name'),
+  lastName: z.string().min(1, 'Required').regex(patterns.name, 'Invalid name'),
+}
 
-export const SignInSchema: ZodType<SignInFields> = z
-  .object({
-    email: z
-      .string({
-        required_error: 'Required',
-      })
-      .min(1, 'Required')
-      .regex(patterns.email, 'Invalid email format')
-      .optional(),
-    phone: z
-      .string({
-        required_error: 'Required',
-      })
-      .regex(
-        patterns.phone,
-        'Invalid format. Should be in the form 0912 345 6789'
-      )
-      .optional(),
-    password: z
-      .string({
-        required_error: 'Required',
-      })
-      .optional(),
-    mode: z.enum(['email', 'phone']),
-  })
-  .refine((data) => (data.mode === 'email' ? !!data.email : !!data.phone), {
-    message: 'Required',
-    path: ['email', 'phone'],
-  })
-  .refine((data) => (data.mode === 'email' ? !!data.password : true), {
-    message: 'Required',
-    path: ['password'],
-  })
+export const SignUpSchema: ZodType<SignUpFields> = z.discriminatedUnion(
+  'mode',
+  [
+    z.object({
+      ...staticFields,
+      mode: z.literal('email'),
+      email: z
+        .string()
+        .min(1, 'Required')
+        .regex(patterns.email, 'Invalid email format'),
+      password: z
+        .string()
+        .min(8, 'Must be 8 characters long')
+        .regex(
+          patterns.password.oneUpperCase,
+          'Must contain at least one uppercase letter'
+        )
+        .regex(
+          patterns.password.oneLowerCase,
+          'Must contain at least one lowercase letter'
+        )
+        .regex(patterns.password.oneDigit, 'Must contain at least one number')
+        .regex(
+          patterns.password.oneSpecialChar,
+          'Must contain at least one special character (.#?!@$%^&*-)'
+        ),
+      phone: z.string().optional(), // Phone is optional for email mode
+    }),
+    z.object({
+      ...staticFields,
+      mode: z.literal('phone'),
+      phone: z
+        .string()
+        .min(1, 'Required')
+        .regex(
+          patterns.phone,
+          'Invalid format. Should be in the form 0912 345 6789'
+        ),
+      email: z.string().optional(),
+      password: z.string().optional(),
+    }),
+  ]
+)
+
+export const SignInSchema: ZodType<SignInFields> = z.discriminatedUnion(
+  'mode',
+  [
+    z.object({
+      mode: z.literal('email'),
+      email: z
+        .string()
+        .min(1, 'Required')
+        .regex(patterns.email, 'Invalid email format'),
+      password: z.string().min(1, 'Required'),
+      phone: z.string().optional(),
+    }),
+    z.object({
+      mode: z.literal('phone'),
+      phone: z
+        .string()
+        .min(1, 'Required')
+        .regex(
+          patterns.phone,
+          'Invalid format. Should be in the form 0912 345 6789'
+        ),
+      email: z.string().optional(),
+      password: z.string().optional(),
+    }),
+  ]
+)
