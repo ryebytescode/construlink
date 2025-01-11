d092ffe3588a896191607c4aadaae21cefb9fb8f
Ryu Rabino <ryebytescode@proton.me>
2025-01-11 13:18:18 +0800

    feat(hooks): add Firebase initializer hook

---
+++ src/hooks/useFirebaseInitializer.ts
@@ -0,0 +1,26 @@
+import { useAuthStore } from '@/stores/auth'
+import auth from '@react-native-firebase/auth'
+import { useEffect, useState } from 'react'
+import { useShallow } from 'zustand/react/shallow'
+
+export function useFirebaseInitializer() {
+  const [isInitializing, setIsInitializing] = useState(true)
+  const { user: currentUser, setUser } = useAuthStore(
+    useShallow((state) => ({
+      user: state.user,
+      setUser: state.setUser,
+    }))
+  )
+
+  function onAuthStateChanged(user: typeof currentUser) {
+    setUser(user)
+    if (isInitializing) setIsInitializing(false)
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth().onAuthStateChanged(onAuthStateChanged)
+    return unsubscribe
+  }, [])
+
+  return isInitializing
+}
