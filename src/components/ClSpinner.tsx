52cd365193e731696d7fc8848e9fc3b4ffbfff27
Ryu Rabino <ryebytescode@proton.me>
2025-01-11 20:11:45 +0800

    feat(components): add ClSpinner

---
+++ src/components/ClSpinner.tsx
@@ -0,0 +1,66 @@
+import { createStyles } from '@/helpers/createStyles'
+import { resolveColor } from '@/helpers/resolveColor'
+import { useAppStore } from '@/stores/app'
+import { Spacing } from '@/theme'
+import { forwardRef, useImperativeHandle, useState } from 'react'
+import { ActivityIndicator, Modal, View } from 'react-native'
+
+interface ClSpinnerProps {
+  transluscent?: boolean
+}
+
+export interface ClSpinnerHandleProps {
+  show: () => void
+  hide: () => void
+}
+
+export const ClSpinner = forwardRef<ClSpinnerHandleProps, ClSpinnerProps>(
+  ({ transluscent }, ref) => {
+    const styles = useStyles({ transluscent })
+    const colors = useAppStore((state) => state.colors)
+    const [isVisible, setIsVisible] = useState(false)
+
+    useImperativeHandle(ref, () => ({
+      show() {
+        setIsVisible(true)
+      },
+      hide() {
+        setIsVisible(false)
+      },
+    }))
+
+    return (
+      <Modal
+        visible={isVisible}
+        transparent={true}
+        animationType="fade"
+        hardwareAccelerated={true}
+      >
+        <View style={styles.container}>
+          <ActivityIndicator
+            size={Spacing[20]}
+            color={resolveColor(colors.accent[500], colors.brand[500])}
+          />
+        </View>
+      </Modal>
+    )
+  }
+)
+
+const useStyles = createStyles(
+  ({ colors }, { transluscent }: { transluscent?: boolean }) => ({
+    container: {
+      backgroundColor: transluscent
+        ? colors.modalBackground
+        : colors.background,
+      justifyContent: 'center',
+      alignItems: 'center',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      zIndex: 99999,
+      width: '100%',
+      height: '100%',
+    },
+  })
+)
