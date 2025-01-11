79b432f3213eaff4018e8aa54f2e0b58f96b0f51
Ryu Rabino <ryebytescode@proton.me>
2025-01-11 23:12:17 +0800

    feat: add sign out functionality to dashboard test screen

--- src/app/(main)/user/dashboard-test.tsx
+++ src/app/(main)/user/dashboard-test.tsx
@@ -1,12 +1,17 @@
 import { ClButton } from '@/components/ClButton'
 import { ClPageView } from '@/components/ClPageView'
 import { ClText } from '@/components/ClText'
+import auth from '@react-native-firebase/auth'
 
 export default function DashboardTestScreen() {
+  async function handleSignOut() {
+    await auth().signOut()
+  }
+
   return (
     <ClPageView id="dashboard-test" title="Welcome!">
       <ClText>You're signed in</ClText>
-      <ClButton text="Sign out" />
+      <ClButton text="Sign out" onPress={handleSignOut} />
     </ClPageView>
   )
 }
