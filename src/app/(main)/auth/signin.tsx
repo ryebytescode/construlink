7fb459b31c04b46388daba9a75effb5edeb337bc
Ryu Rabino <ryebytescode@proton.me>
2025-01-11 23:15:09 +0800

    feat: add sign in functionality

--- src/app/(main)/auth/signin.tsx
+++ src/app/(main)/auth/signin.tsx
@@ -1,43 +1,73 @@
 import { ClButton } from '@/components/ClButton'
 import { ClLinkText } from '@/components/ClLinkText'
 import { ClPageView } from '@/components/ClPageView'
+import { ClSpinner, type ClSpinnerHandleProps } from '@/components/ClSpinner'
 import { ClText } from '@/components/ClText'
 import { ControlledTextInput } from '@/components/controlled/ControlledTextInput'
-import { SignUpSchema } from '@/lib/schemas'
+import { SignInSchema } from '@/lib/schemas'
 import { useAuthStore } from '@/stores/auth'
 import { Spacing } from '@/theme'
 import { IconSet } from '@/types/icons'
 import { zodResolver } from '@hookform/resolvers/zod'
+import auth from '@react-native-firebase/auth'
 import { router } from 'expo-router'
-import React, { type MouseEvent, useState } from 'react'
+import type { FirebaseError } from 'firebase/app'
+import React, { type MouseEvent, useEffect, useRef, useState } from 'react'
 import { type SubmitHandler, useForm } from 'react-hook-form'
-import { type GestureResponderEvent, View } from 'react-native'
+import { Alert, type GestureResponderEvent, View } from 'react-native'
 
-const defaultValues: SignUpFields = {
+const defaultValues: SignInFields = {
   email: '',
-  firstName: '',
-  lastName: '',
   password: '',
   phone: '',
   mode: 'email',
 }
 
 export default function EmailPhoneAuthScreen() {
-  const [inputMode, setInputMode] = useState<SignUpFields['mode']>(
+  const [inputMode, setInputMode] = useState<SignInFields['mode']>(
     defaultValues.mode
   )
-  const { control, handleSubmit, reset } = useForm<SignUpFields>({
-    defaultValues: {
-      ...defaultValues,
-      mode: inputMode,
-    },
-    resolver: zodResolver(SignUpSchema),
+  const { control, handleSubmit, reset, setValue } = useForm<SignInFields>({
+    defaultValues,
+    resolver: zodResolver(SignInSchema),
   })
   const setAuthMode = useAuthStore((state) => state.setMode)
   const setRole = useAuthStore((state) => state.setRole)
   const isEmailMode = inputMode === 'email'
 
-  const onSubmit: SubmitHandler<SignUpFields> = async (data) => {}
+  const spinnerRef = useRef<ClSpinnerHandleProps>(null)
+
+  const onSubmit: SubmitHandler<SignInFields> = async (data) => {
+    spinnerRef.current?.show()
+
+    try {
+      if (isEmailMode) {
+        await auth().signInWithEmailAndPassword(data.email!, data.password!)
+      } else {
+        await auth().signInWithPhoneNumber(data.phone!)
+      }
+    } catch (error) {
+      const errorCode = (error as FirebaseError).code
+
+      if (errorCode === 'auth/invalid-credential') {
+        Alert.alert(
+          'Sign in Failed',
+          'Incorrect email or password. Please try again or reset your password.',
+          [
+            {
+              text: 'Retry',
+              isPreferred: true,
+            },
+          ],
+          {
+            cancelable: false,
+          }
+        )
+      }
+    } finally {
+      spinnerRef.current?.hide()
+    }
+  }
 
   function handleToggleInputMode() {
     setInputMode((prev) => (prev === 'email' ? 'phone' : 'email'))
@@ -53,6 +83,10 @@ export default function EmailPhoneAuthScreen() {
     router.dismissAll()
     router.navigate('/auth/role-selection')
   }
+
+  useEffect(() => {
+    setValue('mode', inputMode)
+  }, [inputMode])
 
   return (
     <ClPageView id="signin" title="Sign in" contentContainerStyle={{ flex: 1 }}>
@@ -96,6 +130,7 @@ export default function EmailPhoneAuthScreen() {
             label: 'Phone',
             placeholder: '0912 345 6789',
             inputMode: 'numeric',
+            maxLength: 11,
           }}
         />
       )}
@@ -114,6 +149,7 @@ export default function EmailPhoneAuthScreen() {
           Create one
         </ClLinkText>
       </ClText>
+      <ClSpinner ref={spinnerRef} transluscent />
     </ClPageView>
   )
 }
