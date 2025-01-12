5f5274411912653d012b8dd193811e5c8cc79aff
Ryu Rabino <ryebytescode@proton.me>
2025-01-12 16:20:00 +0800

    test: display user's full name in the test dashboard screen

--- app/src/app/(main)/user/dashboard-test.tsx
+++ app/src/app/(main)/user/dashboard-test.tsx
@@ -4,12 +4,14 @@ import { ClText } from '@/components/ClText'
 import auth from '@react-native-firebase/auth'
 
 export default function DashboardTestScreen() {
+  const user = auth().currentUser
+
   async function handleSignOut() {
     await auth().signOut()
   }
 
   return (
-    <ClPageView id="dashboard-test" title="Welcome!">
+    <ClPageView id="dashboard-test" title={`Welcome, ${user?.displayName}!`}>
       <ClText>You're signed in</ClText>
       <ClButton text="Sign out" onPress={handleSignOut} />
     </ClPageView>
