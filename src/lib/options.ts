7817cd5cff233706ba9d11472e217d4e8864dc7d
Ryu Rabino <ryebytescode@proton.me>
2025-01-12 11:01:38 +0800

    feat: add mail url maps for deep linking into common mail apps

--- src/lib/options.ts
+++ src/lib/options.ts
@@ -12,4 +12,13 @@ export namespace Cl {
       value: Role.EMPLOYER,
     },
   ]
+
+  export const mailAppUrls: Record<string, string> = {
+    'gmail.com': 'https://gmail.app.goo.gl',
+    'yahoo.com': 'https://mail.yahoo.com',
+    'ymail.com': 'https://mail.yahoo.com',
+    'myyahoo.com': 'https://mail.yahoo.com',
+    'outlook.com': 'https://outlook.live.com/mail/0',
+    'hotmail.com': 'https://outlook.live.com/mail/0',
+  }
 }
