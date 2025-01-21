121df8057f4bd11e353105eb87b9d86dfea6efb6
Ryu Rabino <ryebytescode@proton.me>
2025-01-21 13:37:43 +0800

    feat: add job and company API helpers

--- app/src/services/firestore.ts
+++ app/src/services/firestore.ts
@@ -14,3 +14,62 @@ export namespace UserCollection {
     return result.exists ? result.data()!.role : null
   }
 }
+
+export namespace JobCollection {
+  export async function getAllJobPosts() {
+    const entries: Job[] = []
+
+    try {
+      const result = await firestore()
+        .collection<Job>('jobs')
+        .orderBy('createdAt', 'desc')
+        .get()
+
+      if (result.empty) return null
+
+      // biome-ignore lint/complexity/noForEach:
+      result.forEach((documentSnapshot) => {
+        entries.push({
+          ...documentSnapshot.data(),
+          key: documentSnapshot.id,
+        })
+      })
+
+      return entries
+    } catch (error: unknown) {
+      console.error(error)
+      return null
+    }
+  }
+
+  export async function getJobPost(jobId: string) {
+    try {
+      const result = await firestore().collection<Job>('jobs').doc(jobId).get()
+
+      if (!result.exists) return null
+
+      return result.data() as Job
+    } catch (error: unknown) {
+      console.error(error)
+      return null
+    }
+  }
+}
+
+export namespace CompanyCollection {
+  export async function getCompanyDetails(companyId: string) {
+    try {
+      const result = await firestore()
+        .collection('companies')
+        .doc(companyId)
+        .get()
+
+      if (!result.exists) return null
+
+      return result.data() as Company
+    } catch (error: unknown) {
+      console.error(error)
+      return null
+    }
+  }
+}
