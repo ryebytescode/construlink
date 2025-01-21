0b55ce3154c1e9b814ffc260e706465beb082ffb
Ryu Rabino <ryebytescode@proton.me>
2025-01-21 13:29:56 +0800

    feat: add job and company collection schemas

--- app/src/types/collections.d.ts
+++ app/src/types/collections.d.ts
@@ -1,3 +1,33 @@
 interface User {
   role: import('@/lib/constants').Role
 }
+
+type Timestamp = import(
+  '@react-native-firebase/firestore'
+).FirebaseFirestoreTypes.Timestamp
+type HasKey = { key: string }
+
+interface Job extends HasKey {
+  authorId: string
+  createdAt: Timestamp
+  title: string
+  category: string
+  description: string
+  location: string
+  employmentType: string
+  deadline?: Timestamp
+  payAmount?: number
+  payAmountMin?: number
+  payAmountMax?: number
+  rate?: string
+  isUsingRange: boolean
+  postAs: string
+  status: string
+}
+
+interface Company extends HasKey {
+  name: string
+  description: string
+  size: string
+  location: string
+}
