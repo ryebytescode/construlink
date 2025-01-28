d8ce538bbc102ee62ef06e92a273f78ec26fe5f0
Ryu Rabino <ryebytescode@proton.me>
2025-01-28 20:30:10 +0800

    feat(types): add TypeScript definitions for Appwrite environment variables

---
+++ app/src/types/env.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    EXPO_PUBLIC_APPWRITE_API_URL: string
+    EXPO_PUBLIC_APPWRITE_PROJECT_ID: string
+    EXPO_PUBLIC_APPWRITE_PLATFORM: string
+    EXPO_PUBLIC_APPWRITE_PROFILES_BUCKET_ID: string
+  }
+}
