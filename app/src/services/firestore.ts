5db3f43b5c8dba916b1fb7b6fb8d0d2e7b59f9d1
Ryu Rabino <ryebytescode@proton.me>
2025-01-29 10:22:28 +0800

    feat: implement job application functionality with status tracking

--- app/src/services/firestore.ts
+++ app/src/services/firestore.ts
@@ -1,17 +1,38 @@
-import type { Role } from '@/lib/constants'
+import { JobApplicationStatus, type Role } from '@/lib/constants'
 import firestore from '@react-native-firebase/firestore'
+import { v7 as uuidv7 } from 'uuid'
+
+const db = firestore()
 
 export namespace UserCollection {
   export async function setRole(uid: string, role: Role) {
-    return await firestore()
-      .collection<User>('users')
-      .doc(uid)
-      .set({ role: role })
+    return await db.collection<User>('users').doc(uid).set({ role: role })
   }
 
   export async function getRole(uid: string) {
-    const result = await firestore().collection<User>('users').doc(uid).get()
+    const result = await db.collection<User>('users').doc(uid).get()
     return result.exists ? result.data()!.role : null
+  }
+
+  export async function getStats(
+    uid: string,
+    role: Role
+  ): Promise<Stats | null> {
+    try {
+      const result = await db
+        .collection<Job>('jobs')
+        .where('authorId', '==', uid)
+        .get()
+
+      return {
+        posts: result.size,
+        hires: 0,
+        rating: 0,
+      } as Stats
+    } catch (error: unknown) {
+      console.error(error)
+      return null
+    }
   }
 }
 
@@ -20,7 +41,7 @@ export namespace JobCollection {
     const entries: Job[] = []
 
     try {
-      const result = await firestore()
+      const result = await db
         .collection<Job>('jobs')
         .orderBy('createdAt', 'desc')
         .get()
@@ -44,7 +65,7 @@ export namespace JobCollection {
 
   export async function getJobPost(jobId: string) {
     try {
-      const result = await firestore().collection<Job>('jobs').doc(jobId).get()
+      const result = await db.collection<Job>('jobs').doc(jobId).get()
 
       if (!result.exists) return null
 
@@ -54,15 +75,61 @@ export namespace JobCollection {
       return null
     }
   }
+
+  export async function applyJob(uid: string, jobId: string, message?: string) {
+    try {
+      // Update the application count
+      await db
+        .collection<Job>('jobs')
+        .doc(jobId)
+        .update({
+          applyCount: firestore.FieldValue.increment(1),
+        })
+
+      await db
+        .collection<Partial<JobApplication>>('applications')
+        .doc(uuidv7())
+        .set({
+          jobId,
+          status: JobApplicationStatus.PENDING,
+          tradespersonId: uid,
+          message,
+        })
+
+      return true
+    } catch (error: unknown) {
+      console.error(error)
+      return false
+    }
+  }
+
+  export async function checkIfAppliedToJob(uid: string, jobId: string) {
+    try {
+      let isApplied = false
+      const result = await db
+        .collection<JobApplication>('applications')
+        .where('tradespersonId', '==', uid)
+        .get()
+
+      if (result.empty) return false
+
+      // biome-ignore lint/complexity/noForEach:
+      result.forEach((document) => {
+        isApplied = document.data().jobId === jobId
+      })
+
+      return isApplied
+    } catch (error: unknown) {
+      console.error(error)
+      return false
+    }
+  }
 }
 
 export namespace CompanyCollection {
   export async function getCompanyDetails(companyId: string) {
     try {
-      const result = await firestore()
-        .collection('companies')
-        .doc(companyId)
-        .get()
+      const result = await db.collection('companies').doc(companyId).get()
 
       if (!result.exists) return null
 
