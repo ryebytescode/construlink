32edc2e6c7b707797026c07e97eb05bf0db09bcb
Ryu Rabino <ryebytescode@proton.me>
2025-02-15 12:47:55 +0800

    feat: implement authentication context provider for user state management

---
+++ app/src/contexts/auth.tsx
@@ -0,0 +1,72 @@
+import { useRenderCount } from '@/hooks/useRenderCount'
+import { User, UserCollection } from '@/services/firebase'
+import { type FirebaseAuthTypes, getAuth } from '@react-native-firebase/auth'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import {
+  type PropsWithChildren,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
+
+export type AuthMode = 'signin' | 'signup'
+
+interface AuthContextProps {
+  initializing: boolean
+  userInfo: User | null
+  reset(): void
+}
+
+const AuthContext = createContext<AuthContextProps>({} as AuthContextProps)
+
+export function ClAuthProvider({ children }: PropsWithChildren) {
+  useRenderCount('AuthProvider')
+
+  const [initializing, setInitializing] = useState(true)
+  const [isAuth, setIsAuth] = useState(false)
+  const tqClient = useQueryClient()
+  const { data: cachedUserInfo } = useQuery<User>({
+    queryKey: ['user'],
+    queryFn: async () => {
+      const user = User.get()
+      const userInfo = await UserCollection.getUserInfo(user!.uid)
+      if (!userInfo) {
+        throw new Error('User not found')
+      }
+      return userInfo
+    },
+    enabled: isAuth,
+  })
+
+  function reset() {
+    setIsAuth(false)
+    tqClient.clear()
+  }
+
+  function onAuthStateChanged(user: FirebaseAuthTypes.User | null) {
+    setIsAuth(user !== null)
+    if (initializing) setInitializing(false)
+  }
+
+  useEffect(() => {
+    const unsubscribe = getAuth().onAuthStateChanged(onAuthStateChanged)
+    return unsubscribe
+  }, [])
+
+  return (
+    <AuthContext.Provider
+      value={{ initializing, userInfo: cachedUserInfo ?? null, reset }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+  return context
+}
