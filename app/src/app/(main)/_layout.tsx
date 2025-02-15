8178d9affea92f9dfd558d974227b68253b908b1
Ryu Rabino <ryebytescode@proton.me>
2025-02-15 12:35:18 +0800

    refactor: simplify layout structure and improve resource loading

--- app/src/app/(main)/_layout.tsx
+++ app/src/app/(main)/_layout.tsx
@@ -1,36 +1,13 @@
+import { ClSpinner } from '@/components/ClSpinner'
 import { ClStack } from '@/components/navigation/ClStack'
+import { useAuth } from '@/contexts/auth'
 import { createStyles } from '@/helpers/createStyles'
 import { resolveColor } from '@/helpers/resolveColor'
 import { useRenderCount } from '@/hooks/useRenderCount'
+import { Role } from '@/lib/constants'
 import { Toasts } from '@backpackapp-io/react-native-toast'
-import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister'
-import { QueryClient, onlineManager } from '@tanstack/react-query'
-import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
-import * as Network from 'expo-network'
-import { Stack } from 'expo-router'
-import { DevToolsBubble } from 'react-native-react-query-devtools'
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 60 * 24, // 24 hours
-    },
-  },
-})
-
-const asyncStoragePersister = createAsyncStoragePersister({
-  storage: AsyncStorage,
-})
-
-onlineManager.setEventListener((setOnline) => {
-  const subscription = Network.addNetworkStateListener((state) =>
-    setOnline(state?.isConnected || false)
-  )
-
-  return subscription.remove
-})
+import { Stack, router } from 'expo-router'
+import { useEffect } from 'react'
 
 export default function MainLayout() {
   useRenderCount('MainLayout')
@@ -38,120 +15,29 @@ export default function MainLayout() {
   const styles = useStyles()
 
   return (
-    <PersistQueryClientProvider
-      client={queryClient}
-      persistOptions={{ persister: asyncStoragePersister }}
-    >
-      <BottomSheetModalProvider>
-        <ClStack id="main">
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-
-          <Stack.Screen
-            name="auth/role-selection"
-            options={{ headerTitle: () => null }}
-          />
-          <Stack.Screen
-            name="auth/method-chooser"
-            options={{ headerTitle: () => null }}
-          />
-          <Stack.Screen
-            name="auth/signin"
-            options={{ headerTitle: () => null }}
-          />
-          <Stack.Screen
-            name="auth/signup"
-            options={{ headerTitle: () => null }}
-          />
-          <Stack.Screen
-            name="auth/forgot-password"
-            options={{ headerTitle: () => null }}
-          />
-          <Stack.Screen
-            name="auth/email-sent"
-            options={{ headerTitle: () => null }}
-          />
-          <Stack.Screen
-            name="auth/reset-password"
-            options={{ headerTitle: () => null }}
-          />
-          <Stack.Screen
-            name="auth/reset-done"
-            options={{ headerTitle: () => null }}
-          />
-          <Stack.Screen
-            name="auth/verification"
-            options={{ headerTitle: () => null }}
-          />
-          <Stack.Screen
-            name="auth/verification-done"
-            options={{ headerTitle: () => null }}
-          />
-
-          <Stack.Screen name="user/(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen
-            name="user/dashboard-test"
-            options={{ headerTitle: () => null }}
-          />
-          <Stack.Screen
-            name="user/settings/index"
-            options={{ title: 'Account Settings' }}
-          />
-          <Stack.Screen
-            name="user/settings/account"
-            options={{ title: 'Account & Security' }}
-          />
-          <Stack.Screen
-            name="user/settings/edit/display-name"
-            options={{ title: 'Display Name', animation: 'fade_from_bottom' }}
-          />
-          <Stack.Screen
-            name="user/job/search"
-            options={{ headerTitle: () => null, animation: 'fade_from_bottom' }}
-          />
-          <Stack.Screen
-            name="user/job/[jobId]"
-            options={{ headerTitle: () => null }}
-          />
-          <Stack.Screen
-            name="user/job/apply"
-            options={{ title: 'Apply Job' }}
-          />
-          <Stack.Screen
-            name="user/job/apply-done"
-            options={{ headerTitle: () => null }}
-          />
-          <Stack.Screen
-            name="user/job/view-application"
-            options={{ title: 'Application Details' }}
-          />
-          <Stack.Screen
-            name="user/job/create"
-            options={{ title: 'Create Job Post' }}
-          />
-          <Stack.Screen
-            name="user/job/description-editor"
-            options={{ title: 'Edit' }}
-          />
-          <Stack.Screen
-            name="user/job/preview"
-            options={{ title: 'Preview' }}
-          />
-        </ClStack>
-        <Toasts
-          defaultStyle={{
-            pressable: styles.toastPressable,
-            text: styles.toastText,
-          }}
-        />
-      </BottomSheetModalProvider>
-      <DevToolsBubble />
-    </PersistQueryClientProvider>
+    <>
+      <ClStack id="main" initialRouteName="index">
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="auth" options={{ headerShown: false }} />
+        <Stack.Screen name="user" options={{ headerShown: false }} />
+      </ClStack>
+      <Toasts
+        defaultStyle={{
+          pressable: styles.toastPressable,
+          text: styles.toastText,
+        }}
+      />
+    </>
   )
 }
 
-const useStyles = createStyles(({ colors, typo, sizes }) => ({
+const useStyles = createStyles(({ scheme, colors, typo, sizes }) => ({
   toastPressable: {
-    backgroundColor: resolveColor(colors.neutral[600], colors.neutral[100]),
+    backgroundColor: resolveColor(
+      scheme,
+      colors.neutral[600],
+      colors.neutral[100]
+    ),
     borderRadius: sizes.radius['2xl'],
   },
   toastText: {
--- app/src/app/_layout.tsx
+++ app/src/app/_layout.tsx
@@ -1,33 +1,28 @@
-import { ClSpinner } from '@/components/ClSpinner'
-import { useRefresh } from '@/hooks/useRefresh'
+import { ClAuthProvider } from '@/contexts/auth'
+import { ClThemeProvider } from '@/contexts/theme'
 import { useRenderCount } from '@/hooks/useRenderCount'
-import { useScheme } from '@/hooks/useScheme'
-import { Role } from '@/lib/constants'
-import { UserCollection } from '@/services/firebase'
-import { useAppStore } from '@/stores/app'
-import { useAuthStore } from '@/stores/auth'
-import { Palette } from '@/theme'
 import {
   AntDesign,
   Ionicons,
   MaterialCommunityIcons,
   MaterialIcons,
 } from '@expo/vector-icons'
-import auth from '@react-native-firebase/auth'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister'
+import { QueryClient, onlineManager } from '@tanstack/react-query'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
+import { useMount } from 'ahooks'
 import * as Font from 'expo-font'
-import * as NavigationBar from 'expo-navigation-bar'
-import { NetworkStateType, useNetworkState } from 'expo-network'
-import { Slot, router, useSegments } from 'expo-router'
+import * as Network from 'expo-network'
+import { Slot } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
 import { StatusBar } from 'expo-status-bar'
-import * as SystemUI from 'expo-system-ui'
-import React, { useEffect, useState } from 'react'
-import { Alert } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import 'react-native-get-random-values'
+import { KeyboardProvider } from 'react-native-keyboard-controller'
+import { DevToolsBubble } from 'react-native-react-query-devtools'
 import 'react-native-reanimated'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import { useShallow } from 'zustand/react/shallow'
 
 export { ErrorBoundary } from 'expo-router'
 
@@ -38,37 +33,33 @@ SplashScreen.setOptions({
   fade: true,
 })
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 60 * 24, // 24 hours
+    },
+  },
+})
+
+const persister = createAsyncStoragePersister({
+  storage: AsyncStorage,
+})
+
+onlineManager.setEventListener((setOnline) => {
+  const subscription = Network.addNetworkStateListener((state) =>
+    setOnline(state?.isConnected || false)
+  )
+
+  return subscription.remove
+})
+
 export default function RootLayout() {
   useRenderCount('RootLayout')
 
-  const segments = useSegments()
-  const scheme = useAppStore((state) => state.scheme)
-  const changeScheme = useAppStore((state) => state.changeScheme)
-  const { user, role, setUser, setRole } = useAuthStore(
-    useShallow((state) => ({
-      user: state.user,
-      role: state.role,
-      setUser: state.setUser,
-      setRole: state.setRole,
-    }))
-  )
-  const { refresh } = useRefresh()
-  const networkState = useNetworkState()
-  const [isReady, setIsReady] = useState(false)
-
-  useEffect(() => {
-    let unsubscribe: (() => void) | undefined = undefined
-
-    async function initialize() {
-      unsubscribe = auth().onAuthStateChanged(async (currentUser) => {
-        if (currentUser) {
-          setUser(currentUser)
-          setRole(await UserCollection.getRole(currentUser.uid))
-        }
-      })
-
+  useMount(() => {
+    async function loadResources() {
       try {
-        // Cache fonts
+        // Cache icons
         await Font.loadAsync({
           ...MaterialIcons.font,
           ...MaterialCommunityIcons.font,
@@ -78,63 +69,31 @@ export default function RootLayout() {
       } catch (error) {
         if (__DEV__) console.warn(error)
       } finally {
-        setIsReady(true)
+        SplashScreen.hideAsync()
       }
     }
 
-    initialize()
-
-    return unsubscribe
-  }, [])
-
-  useEffect(() => {
-    changeScheme(scheme)
-
-    SystemUI.setBackgroundColorAsync(Palette[scheme].background)
-    NavigationBar.setBackgroundColorAsync(Palette[scheme].background)
-    NavigationBar.setPositionAsync('relative')
-  }, [scheme, changeScheme])
-
-  useEffect(() => {
-    if (isReady) {
-      SplashScreen.hideAsync().then(() => {
-        if (user && role) {
-          if (router.canDismiss()) router.dismissAll()
-          router.replace(
-            role === Role.EMPLOYER ? '/user/tradespeople' : '/user/jobs'
-          )
-        } else if (!user && segments[1] === 'user') {
-          router.replace('/')
-        }
-      })
-    }
-  }, [isReady, user, role])
-
-  if (networkState.type === NetworkStateType.NONE) {
-    Alert.alert(
-      "You're offline",
-      "It seems like there's no internet connection. Please check your network settings and try again.",
-      [
-        {
-          text: 'Retry',
-          isPreferred: true,
-          onPress: refresh,
-        },
-      ],
-      {
-        cancelable: false,
-      }
-    )
-  }
-
-  if (!isReady) return <ClSpinner visible />
+    loadResources()
+  })
 
   return (
     <GestureHandlerRootView>
       <SafeAreaProvider>
-        <Slot />
+        <KeyboardProvider>
+          <PersistQueryClientProvider
+            client={queryClient}
+            persistOptions={{ persister }}
+          >
+            <ClAuthProvider>
+              <ClThemeProvider>
+                <Slot />
+              </ClThemeProvider>
+            </ClAuthProvider>
+            <DevToolsBubble />
+          </PersistQueryClientProvider>
+          <StatusBar style="auto" />
+        </KeyboardProvider>
       </SafeAreaProvider>
-      <StatusBar style="auto" />
     </GestureHandlerRootView>
   )
 }
