9ad85618c51a52362ea94dc82d37e6aeb135d466
Ryu Rabino <ryebytescode@proton.me>
2025-03-06 17:40:42 +0800

    refactor: remove authentication logic and sign-out button from TradespeopleTab

--- app/src/app/(main)/user/(tabs)/tradespeople.tsx
+++ app/src/app/(main)/user/(tabs)/tradespeople.tsx
@@ -1,28 +1,5 @@
-import { ClButton } from '@/components/ClButton'
 import { ClPageView } from '@/components/ClPageView'
-import { ClText } from '@/components/ClText'
-import { useAuthStore } from '@/stores/auth'
-import auth from '@react-native-firebase/auth'
-import { useShallow } from 'zustand/react/shallow'
 
 export default function TradespeopleTab() {
-  const { setUser, setRole } = useAuthStore(
-    useShallow((state) => ({
-      setUser: state.setUser,
-      setRole: state.setRole,
-    }))
-  )
-
-  async function handleSignOut() {
-    await auth().signOut()
-    setUser(null)
-    setRole(null)
-  }
-
-  return (
-    <ClPageView id="tradespeople-tab">
-      <ClText>You're signed in</ClText>
-      <ClButton text="Sign out" onPress={handleSignOut} />
-    </ClPageView>
-  )
+  return <ClPageView id="tradespeople-tab"></ClPageView>
 }
