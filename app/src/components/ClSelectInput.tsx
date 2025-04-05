66da06cf340f9bf4630d6de66575bcd1b1173a19
Ryu Rabino <ryebytescode@proton.me>
2025-04-05 22:18:20 +0800

    feat: enhance ClSelectInput component with loading state and custom bottom sheet support

--- app/src/components/ClSelectInput.tsx
+++ app/src/components/ClSelectInput.tsx
@@ -1,10 +1,17 @@
 import { IconSet } from '@/types/icons'
 import type { BottomSheetModal } from '@gorhom/bottom-sheet'
-import React, { forwardRef, useCallback, useRef, useState } from 'react'
+import React, {
+  forwardRef,
+  type RefObject,
+  useCallback,
+  useRef,
+  useState,
+} from 'react'
 import type { TextInput } from 'react-native'
 import { ClBottomSheet } from './ClBottomSheet'
 import { ClIcon } from './ClIcon'
 import { ClRadioInput, type RadioOption } from './ClRadio'
+import { ClText } from './ClText'
 import { ClTextInput, type ClTextInputProps } from './ClTextInput'
 
 export interface SelectInputOption {
@@ -18,16 +25,30 @@ interface ClSelectInputProps extends Partial<Omit<ClTextInputProps, 'value'>> {
   sheetTitle?: string
   multiple?: boolean
   options: SelectInputOption[]
+  isLoading?: boolean
+  customBottomSheetRef?: RefObject<BottomSheetModal>
 }
 
 export const ClSelectInput = forwardRef<TextInput, ClSelectInputProps>(
   (props, ref) => {
-    const { sheetTitle, options, value, onChangeText, ...rest } = props
+    const {
+      sheetTitle,
+      options,
+      value,
+      onChangeText,
+      isLoading = false,
+      customBottomSheetRef,
+      ...rest
+    } = props
     const bottomSheetRef = useRef<BottomSheetModal>(null)
     const [_value, setValue] = useState(value)
 
     const handleOnPress = useCallback(() => {
-      bottomSheetRef.current?.present()
+      if (customBottomSheetRef) {
+        customBottomSheetRef.current?.present()
+      } else {
+        bottomSheetRef.current?.present()
+      }
     }, [])
 
     const handleOnSelect = useCallback(
@@ -56,27 +77,33 @@ export const ClSelectInput = forwardRef<TextInput, ClSelectInputProps>(
             <ClIcon set={IconSet.MaterialCommunityIcons} name="chevron-right" />
           }
         />
-        <ClBottomSheet
-          ref={bottomSheetRef}
-          title={sheetTitle ?? 'Select'}
-          scrollable
-          // snapPoints={['70%']}
-          contentContainerStyle={{
-            paddingHorizontal: 0,
-          }}
-          enableDynamicSizing={true}
-        >
-          <ClRadioInput
-            id=""
-            value={_value}
-            size="small"
-            options={options}
+        {!customBottomSheetRef && (
+          <ClBottomSheet
+            ref={bottomSheetRef}
+            title={sheetTitle ?? 'Select'}
+            scrollable
+            // snapPoints={['70%']}
             contentContainerStyle={{
-              padding: 0,
+              paddingHorizontal: 0,
             }}
-            onChange={handleOnSelect}
-          />
-        </ClBottomSheet>
+            enableDynamicSizing={true}
+          >
+            {isLoading ? (
+              <ClText>Loading...</ClText>
+            ) : (
+              <ClRadioInput
+                id=""
+                value={_value}
+                size="small"
+                options={options}
+                contentContainerStyle={{
+                  padding: 0,
+                }}
+                onChange={handleOnSelect}
+              />
+            )}
+          </ClBottomSheet>
+        )}
       </>
     )
   }
